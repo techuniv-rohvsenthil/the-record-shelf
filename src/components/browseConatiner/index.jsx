30d91b5c9bc1578d79e94713592542c935f99b7d
Rohini Senthil <rohinivsenthil@gmail.com>
2020-02-18 14:54:07 +0530

    fix: card placed in cardContainer

--- src/components/browseConatiner/index.jsx
+++ src/components/browseConatiner/index.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import * as styles from './index.module.css';
 import useMusicDetails from '../../hooks/useMusicDetails';
 import Card from '../card';
+import CardContainer from '../cardContainer';
 import countryImage from '../../images/001-guitar.png';
 import rockImage from '../../images/012-electric guitar.png';
 import bollywoodImage from '../../images/006-amplifier.png';
@@ -21,18 +22,20 @@ const BrowseContainer = () => {
         <div className={styles.titleText}>
           genres
         </div>
-        <Link to="/pop">
-          <Card text="Pop" img={popImage} alt="pop-icon" />
-        </Link>
-        <Link to="/rock">
-          <Card text="Rock" img={rockImage} alt="rock-icon" />
-        </Link>
-        <Link to="/bollywood">
-          <Card text="Bollywood" img={bollywoodImage} alt="bollywood-icon" />
-        </Link>
-        <Link to="/country">
-          <Card text="Country" img={countryImage} alt="country-icon" />
-        </Link>
+        <CardContainer>
+          <Link to="/pop">
+            <Card text="Pop" img={popImage} alt="pop-icon" />
+          </Link>
+          <Link to="/rock">
+            <Card text="Rock" img={rockImage} alt="rock-icon" />
+          </Link>
+          <Link to="/bollywood">
+            <Card text="Bollywood" img={bollywoodImage} alt="bollywood-icon" />
+          </Link>
+          <Link to="/country">
+            <Card text="Country" img={countryImage} alt="country-icon" />
+          </Link>
+        </CardContainer>
       </div>
     );
   }
--- src/components/cardContainer/index.jsx
+++ src/components/cardContainer/index.jsx
@@ -3,8 +3,10 @@ import propTypes from 'prop-types';
 import * as styles from './index.module.css';
 
 
-const CardConatiner = () => (
-  <div className={styles.container} />
+const CardConatiner = (props) => (
+  <div className={styles.container}>
+    {props.children}
+  </div>
 );
 
 export default CardConatiner;
